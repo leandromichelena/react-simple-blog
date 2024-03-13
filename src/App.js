8ea855c1d0c43a76d9428e847a01ec591251425e
Leandro Michelena <leandromichelena@gmail.com>
2024-03-13 12:16:09 -0400

    Added NavBar with links to each page using react-router-dom Link

--- src/App.js
+++ src/App.js
@@ -4,21 +4,22 @@ import HomePage from './pages/HomePage';
 import AboutPage from './pages/AboutPage';
 import ArticlesListPage from './pages/ArticlesList';
 import ArticlePage from './pages/ArticlePage';
+import NavBar from './NavBar';
 
 function App() {
   return (
     <BrowserRouter>
         <div className="App">
-        <h1>My Simple Blog</h1>
-        <div id='page-body'>
-          <Routes>
-            <Route path='/' element= {<HomePage />} />
-            <Route path='/about' element= {<AboutPage />} />
-            <Route path='/articles' element= {<ArticlesListPage />} />
-            <Route path='/articles/:articleId' element= {<ArticlePage />} />
-          </Routes>
+          <NavBar />
+          <div id='page-body'>
+            <Routes>
+              <Route path='/' element= {<HomePage />} />
+              <Route path='/about' element= {<AboutPage />} />
+              <Route path='/articles' element= {<ArticlesListPage />} />
+              <Route path='/articles/:articleId' element= {<ArticlePage />} />
+            </Routes>
+          </div>
         </div>
-      </div>
     </BrowserRouter>
   );
 }
