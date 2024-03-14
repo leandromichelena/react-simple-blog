72f0a019f60b3c31c182a0fc776c09f6e571f019
Leandro Michelena <leandromichelena@gmail.com>
2024-03-14 16:12:06 -0400

    Created API routes for upvoting and commenting articles on the backend. Installed nodemon as a developer dependency.

--- simple-blog-backend/src/server.js
+++ simple-blog-backend/src/server.js
@@ -1,9 +1,47 @@
 import express from 'express';
 
 const app = express();
+app.use(express.json());
 
-app.get('/hello', (req, res) => {
-    res.send('Hello!');
+const articles = [
+    {
+        "name": "learn-react",
+        "upvotes" : 0,
+        "comments": []
+    }, 
+    {
+        "name": "learn-node",
+        "upvotes": 0,
+        "comments": []
+    },
+    {
+        "name": "mongoDb",
+        "upvotes": 0,
+        "comments": []
+    },
+]
+
+app.put('/api/articles/:name/upvote', (req, res) => {
+    const { name } = req.params;
+    const article = articles.find(article => article.name === name);
+
+    if (article){
+        article.upvotes += 1;
+        res.send(`The article ${article.name} has now ${article.upvotes} upvotes!`);
+    }
+});
+
+app.post('/api/articles/:name/comment', (req, res) => {
+    const { name } = req.params;
+    const { postedBy, text } = req.body;
+    const article = articles.find(article => article.name === name);
+
+    if(article){
+        article.comments.push({postedBy, text})
+        res.send(article.comments);
+    } else {
+        res.send('This article doesn\'t exist');
+    }
 });
 
 app.listen(8000, () => {
